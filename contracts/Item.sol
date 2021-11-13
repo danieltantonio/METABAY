137d6f3a0172499e39e651d8bf1cf8e589db42b4
DanielTAntonio <danieltomantonio@gmail.com>
2021-11-13 03:57:12 -0800

    Removed unneeded code, put put itemOwner and Owner payment in TestStore.sol, renamed item._owner into _itemOwner

--- contracts/Item.sol
+++ contracts/Item.sol
@@ -5,49 +5,30 @@ contract Item {
     uint index;
     uint price;
     string name;
-    uint quantity;
-    address owner;
     address parent;
     uint timeCreated;
     address itemOwner;
 
-    constructor(string memory _name, uint _price, address _owner, address _itemOwner, uint _timeCreated, uint _index, uint _quantity, address _parent) {
+    constructor(string memory _name, uint _price, address _itemOwner, uint _timeCreated, uint _index, address _parent) {
         name = _name;
         price = _price;
-        owner = _owner;
         index = _index;
-        parent = _parent;
-        quantity = _quantity;
         itemOwner = _itemOwner;
         timeCreated = _timeCreated;
     }
 
     function pay(uint _quantity) public payable {
-        require(_quantity >= 1, "Need 1 or more to purchase.");
-        require(_quantity <= quantity, "Not enough in stock.");
-        require(msg.value == price * _quantity, "Must pay specified price.");
-
-        uint comission = msg.value * 5 / 100;
-        uint itemOwnerPay = msg.value - comission;
-
-        (bool itemOwnerSuccess, ) = itemOwner.call{ value: itemOwnerPay }('');
-        require(itemOwnerSuccess, "Payment to Item Owner did not succeed. Please try again.");
-        (bool ownerSuccess, ) = owner.call{ value: comission }('');
-        require(ownerSuccess, "Payment to Owner did not succeed. Please try again.");
-
         (bool parentSuccess, ) = parent.call{ value: 0 }(abi.encodeWithSignature('triggerPayment(uint256,uint256)', index, _quantity));
     }
 
     function editItem(uint _index, address _owner, uint _price, uint _quantity) external {
         require(msg.sender == itemOwner, "You do not have access to edit this item.");
-
         (bool success, ) = parent.call{ value: 0 }(abi.encodeWithSignature('editItem(uint256,address,uint256,uint256)', _index, _owner, _price, _quantity));
 
         require(success, "Call was not successful. Please try again.");
 
         itemOwner = _owner;
         price = _price;
-        quantity = _quantity;
     }
 
     function returnItemOwner() external view returns(address) {
--- contracts/TestStore.sol
+++ contracts/TestStore.sol
@@ -17,7 +17,7 @@ contract TestStore is Ownable {
 
     struct S_Item {
         string _identifier;
-        address _owner;
+        address _itemOwner;
         uint _price;
         uint _quantity;
         uint _timeCreated;
@@ -29,10 +29,10 @@ contract TestStore is Ownable {
     mapping(uint => S_Item) public items;
 
     function createItem(string memory _name, uint _price, uint _quantity) public {
-        Item item = new Item(_name, _price, owner, msg.sender, block.timestamp, index, _quantity, address(this));
+        Item item = new Item(_name, _price, msg.sender, block.timestamp, index, address(this));
 
         items[index]._identifier = _name;
-        items[index]._owner = msg.sender;
+        items[index]._itemOwner = msg.sender;
         items[index]._price = _price;
         items[index]._quantity = _quantity;
         items[index]._timeCreated = block.timestamp;
@@ -43,7 +43,19 @@ contract TestStore is Ownable {
 
     function triggerPayment(uint _itemIndex, uint _quantity) public payable {
         require(msg.sender == address(items[_itemIndex]._item), "Function not called from Item Contract.");
+        require(_quantity >= 1, "Need 1 or more to purchase.");
+        require(_quantity <= items[_itemIndex]._quantity, "Not enough in stock.");
+        require(msg.value == items[_itemIndex]._price * _quantity, "Must pay specified price."); 
+
         uint purchaseIndex = items[_itemIndex]._purchases;
+        uint comission = msg.value * 5 / 100;
+        uint itemOwnerPay = msg.value - comission;
+        address itemOwner = items[_itemIndex]._itemOwner;
+
+        (bool itemOwnerSuccess, ) = itemOwner.call{ value: itemOwnerPay }('');
+        require(itemOwnerSuccess, "Payment did not send successfully. Please try again.");
+        (bool ownerSuccess, ) = owner.call{ value: comission }('');
+        require(ownerSuccess, "Payment did not send successfully. Please try again.");
 
         items[_itemIndex]._payments[purchaseIndex]._item = address(items[_itemIndex]._item);
         items[_itemIndex]._payments[purchaseIndex]._buyer = msg.sender;
@@ -56,9 +68,9 @@ contract TestStore is Ownable {
         items[_itemIndex]._quantity -= _quantity;
     }
 
-    function editItem(uint _index, address _owner, uint _price, uint _quantity) public {
+    function editItem(uint _index, address _itemOwner, uint _price, uint _quantity) public {
         require(msg.sender == address(items[_index]._item), "Funciton not called from Item Contract.");
-        items[_index]._owner = _owner;
+        items[_index]._itemOwner = _itemOwner;
         items[_index]._price = _price;
         items[_index]._quantity = _quantity;
     } 
